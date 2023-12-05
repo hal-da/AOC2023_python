3fc50094e7a7ba85f09b1b6c18863acffb673e22
Daniel Halasz <daniel.halasz@gmx.net>
2023-12-05 11:42:05 +0100

    day05 b extra naive test

--- day05/day05.py
+++ day05/day05.py
@@ -8,9 +8,7 @@ input_map = [m.split(':') for m in input_map]
 seeds = input_map.pop(0)[1].strip().split(' ')
 
 
-def get_mapping(string: str, src: int) -> int:
-    string = string.strip().split('\n')
-    arr = [[int(x) for x in x.split(' ')] for x in string]
+def get_mapping(arr: [], src: int) -> int:
     for nums in arr:
         if nums[1] <= src <= nums[1] + nums[2]:
             return src + nums[0] - nums[1]
@@ -19,27 +17,36 @@ def get_mapping(string: str, src: int) -> int:
 
 def build_map():
     mapped_seeds = []
+    my_arr = []
+    for z in input_map:
+        string = [[int(s) for s in s.split(' ')] for s in z[1].strip().split('\n')]
+        my_arr.append(string)
+
     for seed in seeds:
-        for to_map in input_map:
-            seed = get_mapping(to_map[1], int(seed))
+        for to_map in my_arr:
+            print(to_map)
+            seed = get_mapping(to_map, int(seed))
         mapped_seeds.append(seed)
     print(min(mapped_seeds))
 
 
 def puzzle_b_naive():
-    lowest_no = 99999999999999999999999999999999999999999
+    lowest_no = 99999999999999999
+    my_arr = []
+    for z in input_map:
+        arr = [[int(s) for s in s.split(' ')] for s in z[1].strip().split('\n')]
+        my_arr.append(arr)
+
     for i in range(408612163):
-        x = i
-        for to_map in input_map:
-            x = get_mapping(to_map[1], 364807853 + x)
-            # print(x)
+        x = i + 364807853
+        for to_map in my_arr:
+            x = get_mapping(to_map, x)
         if x < lowest_no:
             lowest_no = x
-
     print(lowest_no)
 
 
-#build_map()
+# build_map()
 puzzle_b_naive()
 
 # print(408612163 - 364807853)
