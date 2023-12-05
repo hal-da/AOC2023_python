fa8ec2b595cee8d811bd87f2b8a809577b605c5b
Daniel Halasz <daniel.halasz@gmx.net>
2023-12-05 21:08:58 +0100

    day05 b  still doesnt work. works with test data

--- day04/day04.py
+++ day04/day04.py
@@ -4,11 +4,32 @@ from day04input import input as inp
 # B: CORRECT: 5132675
 arr = [card.split(":")[1].strip().split('|') for card in inp.strip().split('\n')]
 
+# for future reference:
+# https://github.com/evanphoward/AdventOfCode/blob/main/AOC_23/Day4/main.py
+# inp = open("input").read().split("\n")
+# inp = [[[int(y) for y in x.split("|")[0].split(":")[1].split()], [int(y) for y in x.split("|")[1].split()]] for x in inp]
+# ans = 0
+#
+# cards = [1 for _ in range(len(inp))]
+#
+# for i, (winning, have_num) in enumerate(inp):
+#     matches = sum(num in have_num for num in winning)
+#     pts = 0 if matches == 0 else 2 ** (matches - 1)
+#     ans += pts
+#     for j in range(i + 1, i + matches + 1):
+#         cards[j] += cards[i]
+#
+# print("Part 1:", ans)
+# print("Part 2:", sum(cards))
+
+#    DR. TONTAFEL
+#     dataset = [[[a for a in line.split("|")[0].split(": ")[1].strip().split(" ") if a], [b for b in line.split("|")[1].split("\n")[0].strip().split(" ") if b]] for line in f]
+
 
 def get_wining_cards_amount(card):
     card_winning_points = 0
     card[0] = card[0].split(" ")
-    card[1] = [str(x) for x in card[1].strip().split(" ") if x != ""]
+    card[1] = [str(x) for x in card[1].strip().split(" ") if x]
     for winning_nums in card[0]:
         try:
             card[1].index(winning_nums)
@@ -31,7 +52,7 @@ def day04a():
 
 def day04b():
     decks = [1]*len(arr)
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         card_winning_points = get_wining_cards_amount(arr[i])
         for j in range(1,card_winning_points+1):
             decks[i+j] += 1*decks[i]
